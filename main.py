28c47c7217e8cbcc014bc7252634d2525b15f754
Dong <0x317374@gmail.com>
2021-04-17 14:33:52 +0700

    chore: W I P

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import cv2
 import mediapipe as mp
 
-if __name__ == "__main__":
-    mp_drawing = mp.solutions.drawing_utils
-    mp_hands = mp.solutions.hands
-    mp_face_mesh = mp.solutions.face_mesh
 
-    # For webcam input:
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+mp_face_mesh = mp.solutions.face_mesh
+
+
+def handle_hands(hands, image):
+    image.flags.writeable = False
+    results = hands.process(image)
+    image.flags.writeable = True
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            for id, lm in enumerate(hand_landmarks.landmark):
+                h, w, c = image.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+
+
+def handle_faces(face_mesh, image):
+    image.flags.writeable = False
+    results = face_mesh.process(image)
+    image.flags.writeable = True
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mp_drawing.draw_landmarks(
+                image=image,
+                landmark_list=face_landmarks,
+                connections=mp_face_mesh.FACE_CONNECTIONS,
+                landmark_drawing_spec=drawing_spec,
+                connection_drawing_spec=drawing_spec,
+            )
+
+
+if __name__ == "__main__":
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
     cap = cv2.VideoCapture(0)
     with mp_hands.Hands(
@@ -19,45 +51,13 @@ if __name__ == "__main__":
 
             if not success:
                 print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
                 continue
 
-            # Flip the image horizontally for a later selfie-view display, and convert
-            # the BGR image to RGB.
             image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            results = hands.process(image)
-
-            # Draw the hand annotations on the image.
-            image.flags.writeable = True
+            handle_hands(hands, image)
+            handle_faces(face_mesh, image)
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image, hand_landmarks, mp_hands.HAND_CONNECTIONS
-                    )
-
-                    for id, lm in enumerate(hand_landmarks.landmark):
-                        h, w, c = image.shape
-                        cx, cy = int(lm.x * w), int(lm.y * h)
-                        print(id, cx, cy)
-
-            results = face_mesh.process(image)
-
-            if results.multi_face_landmarks:
-                for face_landmarks in results.multi_face_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image=image,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACE_CONNECTIONS,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=drawing_spec,
-                    )
-
-            cv2.imshow("Video", image)
+            cv2.imshow("Camera", image)
 
             if cv2.waitKey(5) & 0xFF == 27:
                 break
