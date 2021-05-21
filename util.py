b9c16e3280c711be2e4ced7cbb1a9e103bb0a1ce
Dong <0x317374@gmail.com>
2021-05-21 11:23:56 +0700

    chore: .

--- util.py
+++ util.py
@@ -4,11 +4,8 @@ import io
 def is_raspberrypi():
     try:
         with io.open("/sys/firmware/devicetree/base/model", "r") as m:
-            print(m.read().lower())
-
             if "raspberry pi" in m.read().lower():
                 return True
     except Exception as e:
-        print(e)
         pass
     return False
