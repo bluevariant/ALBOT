ca128438870283e2d7548367395726217d8f8fcf
Dong <0x317374@gmail.com>
2021-05-21 11:25:46 +0700

    chore: .

--- client.py
+++ client.py
@@ -5,25 +5,32 @@ from gpiozero import Device, LED
 from gpiozero.pins.mock import MockFactory
 from util import is_raspberrypi
 
-print(f"is_raspberrypi: {is_raspberrypi()}")
+# mock pin to test on pc
+if not is_raspberrypi():
+    Device.pin_factory = MockFactory()
+
+# https://gpiozero.readthedocs.io/en/stable/recipes.html#pin-numbering
+GO_LED = LED(17)
+DANCE_LED = LED(18)
 
 API_END_POINT = "http://192.168.137.1:8080/call"
 
-cap = cv2.VideoCapture(0)
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
-    success, image = cap.read()
+    while cap.isOpened():
+        success, image = cap.read()
 
-    if not success:
-        print("Ignoring empty camera frame.")
-        continue
+        if not success:
+            print("Ignoring empty camera frame.")
+            continue
 
-    success, buffer = cv2.imencode(".jpg", image)
+        success, buffer = cv2.imencode(".jpg", image)
 
-    if success:
-        jpg_as_text = base64.b64encode(buffer).decode()
-        r = requests.post(API_END_POINT, json={"image_file": jpg_as_text})
+        if success:
+            jpg_as_text = base64.b64encode(buffer).decode()
+            r = requests.post(API_END_POINT, json={"image_file": jpg_as_text})
 
-        print(f'action: {r.json()["action"]}')
+            print(f'action: {r.json()["action"]}')
 
-cap.release()
+    cap.release()
