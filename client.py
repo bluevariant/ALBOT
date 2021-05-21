d318d4a4aed2b6503cb8ef3ed16b478721cf7f5a
Dong <0x317374@gmail.com>
2021-05-21 11:11:23 +0700

    chore: .

--- client.py
+++ client.py
@@ -2,8 +2,18 @@ import cv2
 import base64
 
 cap = cv2.VideoCapture(0)
-retval, image = cap.read()
-retval, buffer = cv2.imencode(".jpg", image)
-jpg_as_text = base64.b64encode(buffer).decode()
-print(jpg_as_text)
+
+while cap.isOpened():
+    success, image = cap.read()
+
+    if not success:
+        print("Ignoring empty camera frame.")
+        continue
+
+    success, buffer = cv2.imencode(".jpg", image)
+
+    if success:
+        jpg_as_text = base64.b64encode(buffer).decode()
+        print(jpg_as_text)
+
 cap.release()
